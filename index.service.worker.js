4b06f39dc1e6f0dc867629e7bb094cfc4abf7cd5
pino503 <yamada2089@gmail.com>
2026-06-10 13:36:24 +0900

    balance: gentler survival scaling with caps, full heal every 10 kills

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781065220|1822536';
+const CACHE_VERSION = '1781066183|1862246';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
