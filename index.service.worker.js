8b497e60075fe5619199fdac272a5430c2bb985e
pino503 <yamada2089@gmail.com>
2026-06-10 11:57:13 +0900

    fix: fighters always face each other (except during active swing frames)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781059654|1943792';
+const CACHE_VERSION = '1781060232|1780312';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
