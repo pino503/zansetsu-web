ee10e12c546f69362138aaafed6c9e75617ec376
pino503 <yamada2089@gmail.com>
2026-06-10 23:10:52 +0900

    feat: lingering kime pose, per-difficulty survival caps

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781098012|1779566';
+const CACHE_VERSION = '1781100537|1764482';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
