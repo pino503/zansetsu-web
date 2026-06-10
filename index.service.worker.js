e959a82d642f033d2e8611ec83a1ddac235e4f49
pino503 <yamada2089@gmail.com>
2026-06-11 06:56:51 +0900

    fix: milestone kenkaku respect difficulty caps

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781128070|1820308';
+const CACHE_VERSION = '1781128610|1812882';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
