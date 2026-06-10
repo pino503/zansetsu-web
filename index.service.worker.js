c0bebef109ff268821b0a58b5110b4bd291e2f15
pino503 <yamada2089@gmail.com>
2026-06-10 21:45:08 +0900

    feat: range fixes, dismemberment with gore settings, swing variations, precise hit lines

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781088756|1725105';
+const CACHE_VERSION = '1781095264|1734620';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
