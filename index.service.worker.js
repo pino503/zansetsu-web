cfe6de0e1f36bcc40bea64ea27bc0a4215889901
pino503 <yamada2089@gmail.com>
2026-06-10 16:32:04 +0900

    feat: counter lands with flesh-cutting sound layers

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781075918|1763359';
+const CACHE_VERSION = '1781076723|1873531';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
