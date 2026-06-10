0774af988fc22a6eb887088a218c8b3b75e4b421
pino503 <yamada2089@gmail.com>
2026-06-10 19:52:53 +0900

    fix: counter marker points at the vulnerable fighter

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781088225|1719332';
+const CACHE_VERSION = '1781088756|1725105';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
