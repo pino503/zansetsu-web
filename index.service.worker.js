ae0f1e320602c9fa40f3623d274a6120c7a13f48
pino503 <yamada2089@gmail.com>
2026-06-10 17:57:14 +0900

    chore: bump version to 1.1.0

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781081201|1725914';
+const CACHE_VERSION = '1781081803|1809999';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
