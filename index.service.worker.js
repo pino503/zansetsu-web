502e34fad4950975a970c9d3151b4446683c3e31
pino503 <yamada2089@gmail.com>
2026-06-11 08:47:29 +0900

    balance: mobs lose the instant-kill counter; flash grace scales by difficulty

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781133763|1996483';
+const CACHE_VERSION = '1781135130|1797687';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
