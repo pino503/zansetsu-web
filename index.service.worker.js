71b6ed4354fd4d3fa9177f490b9d274c78a86cce
pino503 <yamada2089@gmail.com>
2026-06-10 12:05:04 +0900

    feat: clear injury visuals (severity stains, blood drips, trembling arm, lowered guard)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781060232|1780312';
+const CACHE_VERSION = '1781060703|1705293';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
