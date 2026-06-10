4f2829ba148ae6b34d7d7638e77b0cd6a2281750
pino503 <yamada2089@gmail.com>
2026-06-10 12:59:38 +0900

    feat: just-guard works cross-stance, wider parry window (0.22s), deflect input grace

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781062944|1750016';
+const CACHE_VERSION = '1781063977|1722216';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
