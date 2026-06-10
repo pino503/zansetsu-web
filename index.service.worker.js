828c9bbe8b59809f1d93b9373203b5d21eee3229
pino503 <yamada2089@gmail.com>
2026-06-10 23:45:42 +0900

    feat: secret command to play as the armored warrior

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781101485|1799964';
+const CACHE_VERSION = '1781102587|1734609';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
