14bbacee633ca39d1e85f5ce9fb4634b4c3b5984
pino503 <yamada2089@gmail.com>
2026-06-10 18:30:23 +0900

    feat: kusarigama ranged throw and richer weapon art

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781082629|1707781';
+const CACHE_VERSION = '1781083686|1760009';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
