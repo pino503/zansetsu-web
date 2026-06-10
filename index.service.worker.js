cab27e3d038449e114cf9040a940707101f3e36e
pino503 <yamada2089@gmail.com>
2026-06-10 16:18:39 +0900

    feat: title shows (c) Y.M Apps and v1.0.0

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781072995|1728477';
+const CACHE_VERSION = '1781075918|1763359';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
