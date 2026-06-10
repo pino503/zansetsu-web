cd364556425d672f399b0dc3ebe7e412a2f9d713
pino503 <yamada2089@gmail.com>
2026-06-10 18:53:50 +0900

    fix: smooth naginata blade, flat leg sweep, per-weapon stances

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781083686|1760009';
+const CACHE_VERSION = '1781085011|1813756';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
