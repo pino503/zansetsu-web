d618e75834fa3eab3d879cfc033ca5e2b212b961
pino503 <yamada2089@gmail.com>
2026-06-10 14:53:56 +0900

    feat: tap anywhere to start on touch devices

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781070020|1864593';
+const CACHE_VERSION = '1781070695|1895497';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
