439f52de22cb169ebb525ee52f0b1278b0638fa6
pino503 <yamada2089@gmail.com>
2026-06-10 13:14:11 +0900

    feat: heavy attacks crush standing guard; deflect is now a timed dodge (step)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781063977|1722216';
+const CACHE_VERSION = '1781064850|1794778';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
