7eea32d0eebee1fadcbd4e671fd5930eda8c96af
pino503 <yamada2089@gmail.com>
2026-06-11 08:51:52 +0900

    balance: hard flash grace raised to human-reactable 0.20s

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781135130|1797687';
+const CACHE_VERSION = '1781135511|1814170';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
