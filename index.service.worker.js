8d715a86115185aa3b6ab926bc74ad7b2be23089
pino503 <yamada2089@gmail.com>
2026-06-11 07:09:47 +0900

    feat: counter-play kit - feint, step reset, mikiri-gaeshi

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781128610|1812882';
+const CACHE_VERSION = '1781129267|1821994';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
