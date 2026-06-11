711af5779f9c0ed6fae5d91cf00e6a87122b1684
pino503 <yamada2089@gmail.com>
2026-06-11 17:04:13 +0900

    balance: no mob counters in survival

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781164329|1855756';
+const CACHE_VERSION = '1781165050|1831520';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
