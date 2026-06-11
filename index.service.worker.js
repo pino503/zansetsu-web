553305e3b0a1fd0165227876cd7e421a46898c88
pino503 <yamada2089@gmail.com>
2026-06-11 19:38:07 +0900

    v1.3.0

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781173696|1940599';
+const CACHE_VERSION = '1781174284|1839314';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
