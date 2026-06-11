b5dec357f12ebcdbd3753d09c79341c8541a7645
pino503 <yamada2089@gmail.com>
2026-06-11 16:06:10 +0900

    balance: greatsword buff + parry-numb

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781160646|1802429';
+const CACHE_VERSION = '1781161567|1833088';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
