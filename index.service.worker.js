2955fcc8ae9cbb1ea90723d5c0b3884a49418fe4
pino503 <yamada2089@gmail.com>
2026-06-11 14:20:11 +0900

    fix: specials no longer fizzle on low stamina

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781152967|1837964';
+const CACHE_VERSION = '1781155207|1880060';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
