28a01bcadfbe400a48a15bd7aef63f624bca6926
pino503 <yamada2089@gmail.com>
2026-06-11 16:52:15 +0900

    balance: survival mobs weakened

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781162456|1830668';
+const CACHE_VERSION = '1781164329|1855756';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
