8548b8052ada76b004516357568027109f247e20
pino503 <yamada2089@gmail.com>
2026-06-11 19:28:19 +0900

    feat: name guard messages

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781166652|1803856';
+const CACHE_VERSION = '1781173696|1940599';
 /** @type {string} */
 const CACHE_PREFIX = '斬刹 -ZANSETSU--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
